ab50b64b615c68c6f2754e20fa9cee9bdbe0d784
TestAccount-bit <gerhugwsif@gmail.com>
2020-04-27 01:01:48 +0200

    Arithmetic coding with scaling, Encoding(1)

--- lab2/zad1.c
+++ lab2/zad1.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-#define S 8
-#define ALFABET 3 
+#define S 64
+#define ALFABET 10
 
 int main()
 {
-	char alf[ALFABET] = "abc";
-	char arr[S] = "abccabaa"; //"abccabaaabccabaaabccabaaabccabaaabccabaaabccabaaabccabaa";
-	float pw[S] = {0.5, 0.25, 0.25};
+	FILE* fptr; 
+	char alf[ALFABET] = "abcdefghkl";
+	//char arr[S] = "abccabaa"; //8
+	char arr[S] = "abccabaaabccabaaabccabaaabfcabaaabccabaaabccabaaabccabaaabccabaa";	//64
+	float pw[S] = {0.5, 0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05};
 	float F[S+1];
 	float l = 0;
 	float p = 1;
@@ -22,18 +26,18 @@ int main()
 		F[i+1]=counter;
 	}	
 
-/*	for (int i=0;i<S+1;i++)
-	{
-		printf("%f, ", F[i]);
-	}	
-	printf("\n");
-*/
+	fptr = fopen("troika.txt", "w"); 
+	if (fptr == NULL) 
+	{ 
+		printf("Cannot open file \n"); 
+		exit(0); 
+	}
 	
-	printf("g");
 	float d;
 	float z;
 	for (int i=0;i<S;i++)
 	{
+		
 		//printf("g");
 		int c;
 		//printf("arr[%i] %c\n", i, arr[i]);
@@ -56,9 +60,49 @@ int main()
 		l = l+F[c]*d;
 		z = (l+p)/2;
 		printf("arr[%i]:%c, d:%f, l:%f, p:%f, z:%f\n",i, arr[i], d, l, p, z);
+	
+		int licznik = 0;
+		while(true)
+		{
+			if(l>=0 && p<=0.5)
+			{
+				fprintf(fptr, "0");
+				for(int cs=0;cs<licznik;cs++)
+				{
+					fprintf(fptr, "1");
+				}
+				//printf("case 1\n");
+				l=2*l;
+				p=2*p;
+				licznik=0;
+			}
+			else if(l>=0.5 && p<=1)
+			{
+				fprintf(fptr, "1");
+				for(int cs=0;cs<licznik;cs++)
+				{
+					fprintf(fptr, "0");
+				}
+				l=2*l-1;
+				p=2*p-1;	
+				licznik=0;
+			}
+			else if(l>=0.25 && p<=0.75)
+			{
+				licznik+=1;
+				l=2*l-0.5;				
+				p=2*p-0.5;	
+				//printf("\n");
+				
+			}
+			else 
+				break;			
+		}
+		//printf("~\n");
+		printf("arr[%i]:%c, d:%f, l:%f, p:%f, z:%f\n",i, arr[i], d, l, p, z);
 	}	
-	
-	
+	fclose(fptr);
+	/*
 	char out[S] = "";
 	l=0; p=1; int c;
 	for(int i=0;i<S;i++)
@@ -90,6 +134,6 @@ int main()
 	}
 	printf("%c \n%c \n%c \n%c string\n", out[0], out[1], out[2], alf[0]);
 	printf("%s\n", out);
-	
+	*/
 	return 0;
 }
