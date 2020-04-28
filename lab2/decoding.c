3e58a622ac28b27d6fe23c89e3eb94986c55a05a
TestAccount-bit <gerhugwsif@gmail.com>
2020-04-28 21:49:15 +0200

    Splitting zad1.c to encoding.c & decoding.c

---
+++ lab2/decoding.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define SIZE 256
+
+typedef unsigned long long int ullint;
+typedef unsigned char uchar;
+
+
+
+ullint decode_lli(uchar buf[8]);
+float decode_float(uchar buf[4]);
+
+
+int main()
+{
+	FILE* fdecode;
+	fdecode = fopen("archive.bin", "rb"); 
+	
+	ullint decode_size = -1;
+	uchar decode_buff[8];
+	//fread(&decode_buff, 8, 1, fdecode);
+	fread(decode_buff, sizeof(decode_buff), 1, fdecode);
+	for(int i=0;i<8;i++)
+	{
+		printf("%i: %c(%i)\n", i, decode_buff[i], decode_buff[i]);
+	}	
+	decode_size = decode_lli(decode_buff);	
+	printf("lli: %lli\n", decode_size);
+	
+	
+	float F2[SIZE+1] = {0};
+	uchar decode_buff2[sizeof(float)] = {0};
+	for(int i=0;i<SIZE;i++)
+	{
+		fread(decode_buff2, sizeof(decode_buff2), 1, fdecode);
+		F2[i+1] = decode_float(decode_buff2);
+	}
+	
+	for(int i=0;i<SIZE+1;i++)
+	{
+		printf("F2[%i]: %f\n", i, F2[i]);
+	}	
+	
+	fclose(fdecode);	
+	
+	return 0;
+}
+
+ullint decode_lli(uchar buf[8])
+{
+	ullint n = 0;
+	ullint temp = 1;
+	for(int i=0;i<8;i++)
+	{
+		n += (buf[7-i])*temp;
+		temp*=256;
+	}
+	return n;
+}
+
+float decode_float(uchar buf[4])
+{
+	float f1 = *(float*)(buf);
+	return f1;	
+}
+
+
--- lab2/encoding.c
+++ lab2/encoding.c
@@ -5,16 +5,12 @@
 #include <stdlib.h>
 
 #define SIZE 256
-#define ALFABET 10
 
 typedef unsigned long long int ullint;
 typedef unsigned char uchar;
 
 
 void encode_lli(ullint n, uchar buf[8]);
-ullint decode_lli(uchar buf[8]);
-
-float decode_float(uchar buf[4]);
 void encode_float(float n, uchar buf[4]);
 
 
@@ -157,41 +153,12 @@ int main()
 	}	
 	fclose(output);
 	
-	FILE* fdecode;
-	fdecode = fopen("archive.bin", "rb"); 
-	
-	ullint decode_size = -1;
-	uchar decode_buff[8];
-	//fread(&decode_buff, 8, 1, fdecode);
-	fread(decode_buff, sizeof(decode_buff), 1, fdecode);
-	for(int i=0;i<8;i++)
-	{
-		printf("%i: %c(%i)\n", i, decode_buff[i], decode_buff[i]);
-	}	
-	decode_size = decode_lli(decode_buff);	
-	printf("lli: %lli\n", decode_size);
-	
-	
-	float F2[SIZE+1] = {0};
-	uchar decode_buff2[sizeof(float)] = {0};
-	for(int i=0;i<SIZE;i++)
-	{
-		fread(decode_buff2, sizeof(decode_buff2), 1, fdecode);
-		F2[i+1] = decode_float(decode_buff2);
-	}
-	/*
-	for(int i=0;i<SIZE+1;i++)
-	{
-		printf("F2[%i]: %f\n", i, F2[i]);
-	}	
-	*/
-	
 	
 	//sprintf(decode_buff, "%08lli", decode_size);
 	//sscanf(decode_buff, "%08lli", &decode_size);
 	
 	//printf("size decode: %lli\n", decode_size);
-	fclose(fdecode);
+	
 	
 	
 	/*
@@ -242,24 +209,6 @@ void encode_lli(ullint n, uchar buf[8])
 		buf[7-i] = n%256;
 		n /= 256;
 	}
-}
-
-ullint decode_lli(uchar buf[8])
-{
-	ullint n = 0;
-	ullint temp = 1;
-	for(int i=0;i<8;i++)
-	{
-		n += (buf[7-i])*temp;
-		temp*=256;
-	}
-	return n;
-}
-
-float decode_float(uchar buf[4])
-{
-	float f1 = *(float*)(buf);
-	return f1;	
 }
 
 void encode_float(float n, uchar buf[4])
